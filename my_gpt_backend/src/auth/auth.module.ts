8c7df77c91e31f19b2b5c82cbefb7e1a3e150386
edmundchan70 <edmundchan70@gmail.com>
2023-07-20 12:17:12 -0400

    finished setting up docker, db and prisma

--- my_gpt_backend/src/auth/auth.module.ts
+++ my_gpt_backend/src/auth/auth.module.ts
@@ -1,12 +1,4 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { localStrategy } from './local.strategy';
 
-@Module({
-    imports: [PassportModule],
-    controllers: [AuthController],
-    providers: [AuthService, localStrategy],
-})
+@Module({})
 export class AuthModule {}
