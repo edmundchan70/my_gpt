0e829d08f8979788f91beca5135e5bd2fad5aeba
edmundchan70 <edmundchan70@gmail.com>
2023-07-21 01:13:26 -0400

    set up init login front end

--- Front_End/app/src/view/Login/index.tsx
+++ Front_End/app/src/view/Login/index.tsx
@@ -1,59 +1,54 @@
-import { Configuration, OpenAIApi } from 'openai';
-import React, { useRef } from 'react'
-import { handle_error } from '../../api/error/handleError';
-import { AiOutlineSend } from 'react-icons/ai';
-import { useNavigate } from 'react-router-dom';
+import { Avatar, Box, Button, Container, FormControl, Grid, Paper, SxProps, TextField, Theme, colors } from "@mui/material"
 
+import LockIcon from '@mui/icons-material/Lock';
+import { useRef } from "react";
 
 type Props = {}
 
 function Login({}: Props) {
-    const nav = useNavigate();
-    //right now login is just to get the api key 
-    //API_Key should be check using useEffect, then whenever updated , validate and nav HOMEPAGE
-    const getInput = useRef<HTMLInputElement | null>(null);
-    const valid_api = async () : Promise<boolean>  => {
-      const configuration = new Configuration({
-        organization: "org-EVepSWx7EGMJzKHT3eCJVvb4",
-        apiKey: getInput.current!.value ,
-     });
-     const openai = new OpenAIApi(configuration);
-      try{
-      const resp=  await openai.listModels()
-      console.log(resp)
-      return true
-      }catch(error){
-        console.log(error)
-        alert(handle_error(401));
-        return false
-      }};
-    const set_api_key = async ()=>{
-    if(!getInput.current){
-        alert("PLEASE ENTER YOUR API KEY");return ;}
+  const Box_sx : SxProps<Theme> ={
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:"10vh"
+  }
+  const Container_sx : SxProps<Theme> ={
+    display:'flex',
+    flexDirection:"column",
+    padding:'2rem',
+    justifyContent:'center',
+    alignItems:'center'
+  }
+  const Paper_sx : SxProps<Theme> ={
+    width:"30%",
+    height:"500px"
+  }
+  const emailRef = useRef<HTMLInputElement |null>(null);
+  const passwordRef = useRef<HTMLInputElement |null>(null);
 
-    const response = await valid_api()
-  
-      // at last , set api_key, might need encryption future
-    if(response)
-      localStorage.setItem('api_key',getInput.current!.value)
-      console.log("SET")
-        nav("/Home")
-    }
-    //MUST VALID THE API KEY THEN REDIRECT TO PLAYGROUND 
-    return (
-      <div className="App">
-         <div className="max-w-lg mx-auto md:flex md:h-screen md:items-center mb-6 flex">
-           <input
-             className="w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-             type="text"
-             ref={getInput}
-             placeholder="Enter API Key"
-             onSubmit={set_api_key}
-           />
-           <button type='submit' onClick={set_api_key} ><AiOutlineSend size={'2.5rem'}/></button>
-         </div> 
-        
-      </div>)
+      return(
+      <>
+        <Box sx={Box_sx}>
+              <Paper elevation={3} sx={Paper_sx}>
+                  <Container sx={Container_sx}>
+                          <Avatar sx={{bgcolor:colors.pink[400],padding:'1rem'}}>
+                            <LockIcon  />
+                          </Avatar>
+                          <FormControl>
+                            <TextField   label="Email" variant="outlined" />              
+                            <TextField   label="Password" variant="outlined" /> 
+                          </FormControl>
+                          <Button fullWidth variant="contained">Submit</Button>
+                  </Container>
+                  
+                      
+                   
+             
+              </Paper>
+
+        </Box>
+      </>
+    )
  
 }
 
