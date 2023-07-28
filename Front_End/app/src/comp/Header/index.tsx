a8a22e2e3363f18ab285b308dbd281213c89b49a
edmundchan70 <edmundchan70@gmail.com>
2023-07-28 17:08:47 -0400

    save point for setting up navbar, work on dashboard

--- Front_End/app/src/comp/Header/index.tsx
+++ Front_End/app/src/comp/Header/index.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Avatar, Button, IconButton, Toolbar, Typography } from '@mui/material'
+import { AppBar, Avatar, Box, Button, Container, IconButton, Toolbar, Typography, colors } from '@mui/material'
 import React from 'react'
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import Cookies from 'js-cookie';
@@ -14,14 +14,22 @@ function Header({}: Props) {
   }
   return (
 <AppBar position="static">
-  <Toolbar variant="dense" sx={{display:'flex',justifyContent:"space-between"}}>
   
-    <Typography variant="h6" color="inherit" component="div">
-      Chat PDF
-    </Typography>
-    <Button variant='contained' onClick={logout} sx={{backgroundColor:"white"}}>
-      <ExitToAppIcon sx={{color:"darkblue"}}/>
+  <Toolbar variant="dense" sx={{display:'flex',justifyContent:'space-between'}} >
+      <Typography variant="h6" color="inherit" component="div">
+          Chat PDF
+      </Typography>
+    <Box>
+      <Button variant='outlined' sx={{color:"white","&:hover":{bgcolor:"white",color:colors.blue[600]}}}> 
+        DashBoard
+      </Button>
+   
+      <Button variant='outlined' onClick={logout}   sx={{color:"white" ,"&:hover":{bgcolor:"white",color:colors.blue[600]}}}>
+      <ExitToAppIcon/>
     </Button>
+    </Box>
+  
+  
   </Toolbar>
 </AppBar>
   )
